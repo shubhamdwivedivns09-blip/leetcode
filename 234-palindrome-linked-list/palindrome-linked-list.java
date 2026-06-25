fcd6a8a6f460f6d0707d6c6fcd0c1a7979ec0560
Shubham Dwivedi <shubhamdwivedivns09@gmail.com>
2026-06-25 09:36:33 +0530

    Time: 3 ms (99.83%) | Memory: 94.3 MB (78.76%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,47 +1,74 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// class Solution {
+//      public ListNode reverse(ListNode head) {
+//         if(head==null) return null;
+//         ListNode p=null;
+//         ListNode c=head;
+//         while(c!=null){
+//             ListNode f = c.next;
+//             c.next=p;
+//             p=c;
+//             c=f;
+//         }
+//         return p;
+//     }
+//     public boolean isPalindrome(ListNode head) {
+//         if(head.next==null) return true;
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         while(fast.next!=null && fast.next.next!=null){
+//             slow=slow.next;
+//             fast=fast.next.next;
+//         }     
+//         ListNode head2=slow.next;
+//         slow.next=null;
+//         head2=reverse(head2);
+//         ListNode i=head;
+//         ListNode j=head2;
+//         while(j!=null){
+//             if(i.val!=j.val){
+//                 return false;
+//             }
+//             i=i.next;
+//             j=j.next;
+//         }
+//         return true;
+
+//     }
+// }
+
 class Solution {
-     public ListNode reverse(ListNode head) {
-        if(head==null) return null;
-        ListNode p=null;
-        ListNode c=head;
-        while(c!=null){
-            ListNode f = c.next;
-            c.next=p;
-            p=c;
-            c=f;
-        }
-        return p;
-    }
     public boolean isPalindrome(ListNode head) {
-        if(head.next==null) return true;
+        if (head == null || head.next == null)
+            return true;
+
+        // Find middle
         ListNode slow = head;
         ListNode fast = head;
-        while(fast.next!=null && fast.next.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }     
-        ListNode head2=slow.next;
-        slow.next=null;
-        head2=reverse(head2);
-        ListNode i=head;
-        ListNode j=head2;
-        while(j!=null){
-            if(i.val!=j.val){
-                return false;
-            }
-            i=i.next;
-            j=j.next;
-        }
-        return true;
 
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second != null) {
+            if (first.val != second.val)
+                return false;
+
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
     }
 }
