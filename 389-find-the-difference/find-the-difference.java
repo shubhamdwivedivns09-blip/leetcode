d6b02b829997fb75a745fab903650e4566abe241
Shubham Dwivedi <shubhamdwivedivns09@gmail.com>
2026-07-13 10:40:33 +0530

    Time: 3 ms (41.20%) | Memory: 43.2 MB (53.57%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return b[i];
+            }
+        }
+
+        return b[b.length - 1];
+    }
+}
