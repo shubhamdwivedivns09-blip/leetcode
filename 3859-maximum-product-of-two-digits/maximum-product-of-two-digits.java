edcb1a4cede6a1fb41329d08a2ef9fda867b3af0
Shubham Dwivedi <shubhamdwivedivns09@gmail.com>
2026-07-25 07:02:53 +0530

    Time: 2 ms (29.84%) | Memory: 42.9 MB (23.31%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -1,17 +1,13 @@
 class Solution {
     public int maxProduct(int n) {
-        int max1 = 0;
-        int max2 = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            if (digit >= max1) {
-                max2 = max1;
-                max1 = digit;
-            } else if (digit > max2) {
-                max2 = digit;
-            }
-            n /= 10;
+        ArrayList<Integer> l1= new ArrayList<>();
+        while(n>0){
+           int rem=n%10;
+            n=n/10;
+            l1.add(rem);
         }
-        return max1 * max2;
+        Collections.sort(l1);
+        int x= l1.size();
+        return l1.get(x-1)*l1.get(x-2);
     }
 }
